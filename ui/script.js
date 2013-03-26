2cd9e2f2c5c3f89a05e3e42a229658470f3c66ab
Gabriel Dunne <gdunne@quilime.com>
2013-03-26 14:58:21 -0700

    99 particles testing.

--- ui/script.js
+++ ui/script.js
@@ -14,10 +14,10 @@ var color2 = '#07AD96';
 var color3 = '#09979E';
 
 
-var SEPARATION = 50, AMOUNTX = 20, AMOUNTY = 20;
 var container, stats;
 var camera, scene, renderer;
 var particles, particle, count = 0;
+var num_particles = 99;
 var mouseX = 0, mouseY = 0;
 var windowHalfX, windowHalfY;
 var PI2 = Math.PI * 2;
@@ -50,16 +50,13 @@ var init = function() {
   windowHalfX = window.innerWidth / 2;
   windowHalfY = window.innerHeight / 2;
 
-  container = document.createElement( 'div' );
-  document.body.appendChild( container );
-
+  // create camera
   camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
   camera.position.z = 700;
 
   scene = new THREE.Scene();
 
-  particles = new Array();
-
+  // particle material
   var material = new THREE.ParticleCanvasMaterial( {
     color: color3,
     program: function ( context ) {
@@ -69,118 +66,101 @@ var init = function() {
     }
   } );
 
-  var i = 0;
-  for ( var ix = 0; ix < AMOUNTX; ix ++ ) {
-    for ( var iy = 0; iy < AMOUNTY; iy ++ ) {
-      particle = particles[ i ++ ] = new THREE.Particle( material );
-      particle.position.x = ix * SEPARATION - ( ( AMOUNTX * SEPARATION ) / 2 );
-      particle.position.z = iy * SEPARATION - ( ( AMOUNTY * SEPARATION ) / 2 );
-      scene.add( particle );
-    }
+  // create particles
+  particles = new Array();
+  for ( var i = 0; i < num_particles; i ++ ) {
+    particle = particles[ i ++ ] = new THREE.Particle( material );
+    particle.position.x = 0;
+    particle.position.y = 0;
+    scene.add( particle );
   }
 
+  // set up canvas container
+  container = document.createElement( 'div' );
+  document.body.appendChild( container );
+
+  // set up renderer
   renderer = new THREE.CanvasRenderer();
   renderer.setSize( window.innerWidth, window.innerHeight );
   container.appendChild( renderer.domElement );
 
+  // event listeners
+  document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+  document.addEventListener( 'touchstart', onDocumentTouchStart, false );
+  document.addEventListener( 'touchmove', onDocumentTouchMove, false );
+  window.addEventListener( 'resize', onWindowResize, false );
+
+  // stats
   stats = new Stats();
   stats.domElement.style.position = 'absolute';
   stats.domElement.style.top = '0px';
   container.appendChild( stats.domElement );
+};
 
-  document.addEventListener( 'mousemove', onDocumentMouseMove, false );
-  document.addEventListener( 'touchstart', onDocumentTouchStart, false );
-  document.addEventListener( 'touchmove', onDocumentTouchMove, false );
+var animate = function() {
+  requestAnimationFrame( animate );
+  render();
+  stats.update();
+};
 
-  //
+var render = function () {
 
-  window.addEventListener( 'resize', onWindowResize, false );
+  /*
+
+  var rot = 0;
+  var inc = PI2 / 60;
+  var radius = 1000;
+  pos.x + radius * Math.cos(inc * i) * rot;
+  pos.y + radius * Math.sin(inc * i) * rot;
+
+  */
+
+  camera.position.x += ( mouseX - camera.position.x ) * .05;
+  camera.position.y += ( - mouseY - camera.position.y ) * .05;
+  camera.lookAt( scene.position );
+
+  var inc = PI2 / 360;
+  var pos = { x : 0, y : 0};
+  var radius = 500;
+  for ( var i = 0; i < num_particles; i++ ) {
+    particle = particles[ i++ ];
+    particle.position.x = pos.x + Math.cos(inc * i) * radius * 1.15;
+    particle.position.y = pos.y + Math.sin(inc * i) * radius * 1.0;
+    particle.scale.x = particle.scale.y = 6;
+//    particle.scale.x = particle.scale.y = ( Math.sin( ( i + count ) * 0.3 ) + 1 ) * 2 + ( Math.sin( ( i + count ) * 0.5 ) + 1 ) * 2;
+  }
+  renderer.render( scene, camera );
+
+  count += 0.1;
 
 };
 
 
+var onWindowResize = function () {
+  windowHalfX = window.innerWidth / 2;
+  windowHalfY = window.innerHeight / 2;
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize( window.innerWidth, window.innerHeight );
+};
 
+var onDocumentMouseMove = function ( event ) {
+  mouseX = event.clientX - windowHalfX;
+  mouseY = event.clientY - windowHalfY;
+}
 
-function onWindowResize() {
+var onDocumentTouchStart = function ( event ) {
+  if ( event.touches.length === 1 ) {
+    event.preventDefault();
+    mouseX = event.touches[ 0 ].pageX - windowHalfX;
+    mouseY = event.touches[ 0 ].pageY - windowHalfY;
+  }
+};
 
-        windowHalfX = window.innerWidth / 2;
-        windowHalfY = window.innerHeight / 2;
-
-        camera.aspect = window.innerWidth / window.innerHeight;
-        camera.updateProjectionMatrix();
-
-        renderer.setSize( window.innerWidth, window.innerHeight );
-
-      }
-
-      //
-
-      function onDocumentMouseMove( event ) {
-
-        mouseX = event.clientX - windowHalfX;
-        mouseY = event.clientY - windowHalfY;
-
-      }
-
-      function onDocumentTouchStart( event ) {
-
-        if ( event.touches.length === 1 ) {
-
-          event.preventDefault();
-
-          mouseX = event.touches[ 0 ].pageX - windowHalfX;
-          mouseY = event.touches[ 0 ].pageY - windowHalfY;
-
-        }
-
-      }
-
-      function onDocumentTouchMove( event ) {
-
-        if ( event.touches.length === 1 ) {
-
-          event.preventDefault();
-
-          mouseX = event.touches[ 0 ].pageX - windowHalfX;
-          mouseY = event.touches[ 0 ].pageY - windowHalfY;
-
-        }
-
-      }
-
-      //
-
-      function animate() {
-
-        requestAnimationFrame( animate );
-
-        render();
-        stats.update();
-
-      }
-
-      function render() {
-
-        camera.position.x += ( mouseX - camera.position.x ) * .05;
-        camera.position.y += ( - mouseY - camera.position.y ) * .05;
-        camera.lookAt( scene.position );
-
-        var i = 0;
-
-        for ( var ix = 0; ix < AMOUNTX; ix ++ ) {
-
-          for ( var iy = 0; iy < AMOUNTY; iy ++ ) {
-
-            particle = particles[ i++ ];
-            particle.position.y = ( Math.sin( ( ix + count ) * 0.3 ) * 50 ) + ( Math.sin( ( iy + count ) * 0.5 ) * 50 );
-            particle.scale.x = particle.scale.y = ( Math.sin( ( ix + count ) * 0.3 ) + 1 ) * 2 + ( Math.sin( ( iy + count ) * 0.5 ) + 1 ) * 2;
-
-          }
-
-        }
-
-        renderer.render( scene, camera );
-
-        count += 0.1;
-
-      }
+var onDocumentTouchMove = function ( event ) {
+  if ( event.touches.length === 1 ) {
+    event.preventDefault();
+      mouseX = event.touches[ 0 ].pageX - windowHalfX;
+      mouseY = event.touches[ 0 ].pageY - windowHalfY;
+    }
+};
