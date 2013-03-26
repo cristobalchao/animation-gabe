ecd1f168bed22d0314704b31b06b77cdcc59c3dc
Gabriel Dunne <gdunne@quilime.com>
2013-03-26 16:41:44 -0700

    Getting logo set up

--- ui/script.js
+++ ui/script.js
@@ -16,7 +16,7 @@ var color3 = '#09979E';
 
 var container, stats;
 var camera, scene, renderer;
-var particles, particle, count = 0;
+var particles, particles_2, particle, particle_2, count = 0;
 
 
 var num_particles_per_arc = 99;
@@ -58,13 +58,21 @@ var init = function() {
 
   // create camera
   camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
-  camera.position.z = 700;
+  camera.position.z = 1500;
 
   scene = new THREE.Scene();
 
   // particle material
   var material = new THREE.ParticleCanvasMaterial( {
     color: color3,
+    program: function ( context ) {
+      context.beginPath();
+      context.arc( 0, 0, 1, 0, PI2, true );
+      context.fill();
+    }
+  } );
+   var material_2 = new THREE.ParticleCanvasMaterial( {
+    color: color1,
     program: function ( context ) {
       context.beginPath();
       context.arc( 0, 0, 1, 0, PI2, true );
@@ -79,6 +87,15 @@ var init = function() {
     particle.position.x = 0;
     particle.position.y = 0;
     scene.add( particle );
+  }
+
+  // test particles
+  particles_2= new Array();
+  for ( var i = 0; i < num_logo_arcs; i ++ ) {
+    particle_2 = particles_2[ i ] = new THREE.Particle( material_2 );
+    particle_2.position.x = 0;
+    particle_2.position.y = 0;
+    scene.add( particle_2 );
   }
 
   // set up canvas container
@@ -111,45 +128,71 @@ var animate = function() {
 
 var render = function () {
 
-  /*
-
-  var rot = 0;
-  var inc = PI2 / 60;
-  var radius = 1000;
-  pos.x + radius * Math.cos(inc * i) * rot;
-  pos.y + radius * Math.sin(inc * i) * rot;
-
-  */
-
-  camera.position.x += ( mouseX - camera.position.x ) * .05;
-  camera.position.y += ( - mouseY - camera.position.y ) * .05;
+  //camera.position.x += ( mouseX - camera.position.x ) * .05;
+  //camera.position.y += ( - mouseY - camera.position.y ) * .05;
   camera.lookAt( scene.position );
 
-  var pos = { x : 0, y : 0};
   var inc = PI2 / 360; // one degree per increment
-  var radius = 500;
+  var arc_radius = 700;
+  var logo_radius = 300;
+  var logo_center = { x : 0, y : 0};
 
-  // for (var i = 0; i < num_logo_arcs; i++) {
-    // one arc
+  var arc_rot_inc = PI2 / num_logo_arcs;
+  var arc_rot = 0;
+
+  var count = 0;
+
+  // loop through all arcs
+  for (var i = 0; i < num_logo_arcs; i++) {
+
+    particle_2 = particles_2[ i ];
+
+    arc_rot = arc_rot_inc * i;
+
+    particle_2.position.x = logo_center.x + Math.cos(arc_rot) * logo_radius,
+    particle_2.position.y = logo_center.y + Math.sin(arc_rot) * logo_radius
+    particle_2.scale.x = particle_2.scale.y = 20;
+
+    // draw each arc
     for ( var a = 0; a < num_particles_per_arc; a++ ) {
-      //console.log('b:' + a);
 
-      particle = particles[ a ];
-      //a++;
-      //console.log('a:' + a);
-      particle.position.x = pos.x + Math.cos(inc * a) * radius * 1.15;
-      particle.position.y = pos.y + Math.sin(inc * a) * radius * 1.0;
+      particle = particles[ count++ ];
+
+      // generate particle position based on scaled ellipse
+      particle.position.x = logo_center.x + Math.cos(inc * a) * arc_radius * 1.15;
+      particle.position.y = logo_center.y + Math.sin(inc * a) * arc_radius * 1.0;
+
+      // do rotation per arc
+      particle.position = rotateZ(particle.position, arc_rot);
+
+      // individual rotation
+      particle.position.x -= particle_2.position.x;
+      particle.position.y -= particle_2.position.y;
+      particle.position = rotateZ(particle.position, mouseX / 100.0); // get this rotation correct
+      particle.position.x += particle_2.position.x;
+      particle.position.y += particle_2.position.y;
+
+      // particle scale
       particle.scale.x = particle.scale.y = 6;
     }
-  // }
+  }
 
 
   renderer.render( scene, camera );
-
   count += 0.1;
-
 };
 
+
+var rotateZ = function(p, angle) {
+  var q = p;
+  var sina = Math.sin(angle);
+  var cosa = Math.cos(angle);
+  var rx = q.x * cosa - q.y * sina;
+  var ry = q.x * sina + q.y * cosa;
+  p.x = rx;
+  p.y = ry;
+  return p;
+}
 
 var onWindowResize = function () {
   windowHalfX = window.innerWidth / 2;
